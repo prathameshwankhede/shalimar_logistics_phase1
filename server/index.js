c2ec3796be7fda828af29ca3c51f3906b2fb341c
Prathamesh Wankhede <prathmeshwankhede22@gmail.com>
2026-08-28 23:45:08 +0530

    chore: touch server/index.js to trigger Hostinger Passenger reload for sub_indent_no DTO support

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// Hostinger Passenger Process Reload Signal v1.0.1
+// Hostinger Passenger Process Reload Signal v1.0.2 - sub_indent_no DTO support
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
